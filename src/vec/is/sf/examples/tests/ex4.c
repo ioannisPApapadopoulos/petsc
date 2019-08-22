954c3284e28843893a05837b5a4a82f0aab0c1dc
Lawrence Mitchell <lawrence@wence.uk>
2019-08-22 10:11:17 +0100

    SF: Add test of composed SF with sparse leaf space for sfB

--- src/vec/is/sf/examples/tests/ex4.c
+++ src/vec/is/sf/examples/tests/ex4.c
@@ -7,55 +7,76 @@ int main(int argc, char **argv)
 {
   PetscErrorCode     ierr;
   PetscSF            sfA, sfB, sfBA;
-  const PetscInt     nroots = 4, nleaves = 4;
-  PetscInt          *ilocal;
-  PetscSFNode       *iremote;
+  PetscInt           nrootsA, nleavesA, nrootsB, nleavesB;
+  PetscInt          *ilocalA, *ilocalB;
+  PetscSFNode       *iremoteA, *iremoteB;
   Vec                a, b, ba;
   const PetscScalar *arrayR;
   PetscScalar       *arrayW;
-  PetscMPIInt        rank, size;
+  PetscMPIInt        size;
+  PetscInt           i;
+  PetscInt           maxleafB;
+  PetscBool          flag = PETSC_FALSE;
 
   ierr = PetscInitialize(&argc,&argv,NULL,help);if (ierr) return ierr;
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
 
   if (size != 1) SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_USER, "Only coded for one MPI process");
 
   ierr = PetscSFCreate(PETSC_COMM_WORLD, &sfA); CHKERRQ(ierr);
-
   ierr = PetscSFCreate(PETSC_COMM_WORLD, &sfB); CHKERRQ(ierr);
 
-  ierr = PetscMalloc1(nleaves, &ilocal); CHKERRQ(ierr);
-  ierr = PetscMalloc1(nleaves, &iremote); CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-sparse_sfB",&flag,NULL);CHKERRQ(ierr);
 
-  for ( PetscInt i = 0; i < nleaves; i++ ) {
-    ilocal[i] = nleaves - i - 1;
-    iremote[i].rank = 0;
-    iremote[i].index = i;
+  if (flag) {
+    /* sfA permutes indices, sfB has sparse leaf space. */
+    nrootsA = 3;
+    nleavesA = 3;
+    nrootsB = 3;
+    nleavesB = 2;
+  } else {
+    /* sfA reverses indices, sfB is identity */
+    nrootsA = nrootsB = nleavesA = nleavesB = 4;
+  }
+  ierr = PetscMalloc1(nleavesA, &ilocalA); CHKERRQ(ierr);
+  ierr = PetscMalloc1(nleavesA, &iremoteA); CHKERRQ(ierr);
+  ierr = PetscMalloc1(nleavesB, &ilocalB); CHKERRQ(ierr);
+  ierr = PetscMalloc1(nleavesB, &iremoteB); CHKERRQ(ierr);
+
+  for (i = 0; i < nleavesA; i++) {
+    iremoteA[i].rank = 0;
+    iremoteA[i].index = i;
+    if (flag) {
+      ilocalA[i] = (i + 1) % nleavesA;
+    } else {
+      ilocalA[i] = nleavesA - i - 1;
+    }
   }
 
-  ierr = PetscSFSetGraph(sfA, nroots, nleaves, ilocal, PETSC_COPY_VALUES, iremote, PETSC_COPY_VALUES); CHKERRQ(ierr);
-
-  for ( PetscInt i = 0; i < nleaves; i++ ) {
-    ilocal[i] = i;
-    iremote[i].rank = 0;
-    iremote[i].index = i;
+  for (i = 0; i < nleavesB; i++) {
+    iremoteB[i].rank = 0;
+    if (flag) {
+      ilocalB[i] = nleavesB - i;
+      iremoteB[i].index = nleavesB - i - 1;
+    } else {
+      ilocalB[i] = i;
+      iremoteB[i].index = i;
+    }
   }
 
-  ierr = PetscSFSetGraph(sfB, nroots, nleaves, ilocal, PETSC_OWN_POINTER, iremote, PETSC_OWN_POINTER); CHKERRQ(ierr);
-
+  ierr = PetscSFSetGraph(sfA, nrootsA, nleavesA, ilocalA, PETSC_OWN_POINTER, iremoteA, PETSC_OWN_POINTER); CHKERRQ(ierr);
+  ierr = PetscSFSetGraph(sfB, nrootsB, nleavesB, ilocalB, PETSC_OWN_POINTER, iremoteB, PETSC_OWN_POINTER); CHKERRQ(ierr);
   ierr = PetscSFSetUp(sfA); CHKERRQ(ierr);
-
   ierr = PetscSFSetUp(sfB); CHKERRQ(ierr);
-
   ierr = PetscObjectSetName((PetscObject)sfA, "sfA");CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject)sfB, "sfB");CHKERRQ(ierr);
 
-  ierr = VecCreateSeq(PETSC_COMM_WORLD, nroots, &a); CHKERRQ(ierr);
-  ierr = VecCreateSeq(PETSC_COMM_WORLD, nleaves, &b); CHKERRQ(ierr);
-  ierr = VecDuplicate(b, &ba); CHKERRQ(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_WORLD, nrootsA, &a); CHKERRQ(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_WORLD, nleavesA, &b); CHKERRQ(ierr);
+  ierr = PetscSFGetLeafRange(sfB, NULL, &maxleafB); CHKERRQ(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_WORLD, maxleafB+1, &ba); CHKERRQ(ierr);
   ierr = VecGetArray(a, &arrayW); CHKERRQ(ierr);
-  for ( PetscInt i = 0; i < nroots; i++ ) {
+  for ( i = 0; i < nrootsA; i++ ) {
     arrayW[i] = (PetscScalar)i;
   }
   ierr = VecRestoreArray(a, &arrayW); CHKERRQ(ierr);
@@ -74,6 +95,7 @@ int main(int argc, char **argv)
 
   ierr = VecGetArrayRead(b, &arrayR); CHKERRQ(ierr);
   ierr = VecGetArray(ba, &arrayW); CHKERRQ(ierr);
+  arrayW[0] = 10.0;             /* Not touched by bcast */
   ierr = PetscSFBcastBegin(sfB, MPIU_SCALAR, arrayR, arrayW); CHKERRQ(ierr);
   ierr = PetscSFBcastEnd(sfB, MPIU_SCALAR, arrayR, arrayW); CHKERRQ(ierr);
   ierr = VecRestoreArrayRead(b, &arrayR); CHKERRQ(ierr);
@@ -83,11 +105,10 @@ int main(int argc, char **argv)
   ierr = VecView(ba, NULL); CHKERRQ(ierr);
 
   ierr = PetscSFCompose(sfA, sfB, &sfBA); CHKERRQ(ierr);
-
   ierr = PetscObjectSetName((PetscObject)sfBA, "(sfB o sfA)");CHKERRQ(ierr);
   ierr = VecGetArrayRead(a, &arrayR); CHKERRQ(ierr);
   ierr = VecGetArray(ba, &arrayW); CHKERRQ(ierr);
-
+  arrayW[0] = 11.0;             /* Not touched by bcast */
   ierr = PetscSFBcastBegin(sfBA, MPIU_SCALAR, arrayR, arrayW); CHKERRQ(ierr);
   ierr = PetscSFBcastEnd(sfBA, MPIU_SCALAR, arrayR, arrayW); CHKERRQ(ierr);
   ierr = VecRestoreArray(ba, &arrayW); CHKERRQ(ierr);
@@ -114,6 +135,10 @@ int main(int argc, char **argv)
 /*TEST
 
    test:
+     suffix: 1
+
+   test:
+     suffix: 2
+     args: -sparse_sfB
 
 TEST*/
-
